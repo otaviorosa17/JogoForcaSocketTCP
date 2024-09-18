52572271ec94eaff2a012d1e476e56a883414629
tavin <otavioarosa@usp.br>
2024-09-18 12:40:29 -0300

    apagando teste.txt

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,21 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
-    
+    public static void main(String[] args) throws Exception {
+        String clientSentence; 
+        String capitalizedSentence; 
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("conectou");
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine(); 
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence); 
+        }
+        
+    }
 }
