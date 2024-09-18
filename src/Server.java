699b3516289d4c39203b7dffbd93fd7ce848967a
Diego soares de carvalho Junior <dieguinhosoares2005@gmail.com>
2024-09-18 12:36:50 -0300

    esboco de conexao

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,24 @@
-public class Server {
+import java.io.*;
+import java.net.*;
+
+class Server{
+    public static void main(String argv[]) throws Exception{
+    String clientSentence;
+    String capitalizedSentence;
+
+    ServerSocket welcomeSocket = new ServerSocket(6789);
+
+    while(true) {
+
+      Socket connectionSocket = welcomeSocket.accept();
+      System.out.println("Deu bom!!");
+      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+      DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+      clientSentence = inFromClient.readLine();
+      capitalizedSentence = clientSentence.toUpperCase() + '\n';
+      outToClient.writeBytes(capitalizedSentence); 
     
+      }
+    }
+  
 }
